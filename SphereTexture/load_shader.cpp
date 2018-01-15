32b66bf4a1ca258421e0250d167c5cd45754fbc0
Your Name <you@example.com>
2018-01-15 12:54:36 +0530

    Adding to a project to map an earth texture to a sphere

---
+++ SphereTexture/load_shader.cpp
@@ -0,0 +1,63 @@
+/************************************************************** 
+ *
+ *  Function to load shaders.
+ *
+ *  Author : Mathew P Joseph
+ *  Email  : mathew.p.joseph@gmail.com
+ *
+ *************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define GLFW_INCLUDE_ES2
+#include <GLFW/glfw3.h>
+
+GLint common_get_shader_program(const char *vertex_shader_source, const char *fragment_shader_source) 
+{
+
+    enum Consts {INFOLOG_LEN = 512};
+    GLchar infoLog[INFOLOG_LEN];
+    GLint fragment_shader;
+    GLint shader_program;
+    GLint success;
+    GLint vertex_shader;
+
+    /* Vertex shader */
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, INFOLOG_LEN, NULL, infoLog);
+        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", infoLog);
+    }
+
+    /* Fragment shader */
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, INFOLOG_LEN, NULL, infoLog);
+        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
+    }
+
+    /* Link shaders */
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader_program, INFOLOG_LEN, NULL, infoLog);
+        printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    return shader_program;
+}
+
---
+++ SphereTexture/matrix.h
@@ -0,0 +1,7 @@
+#define GLFW_INCLUDE_ES2
+#include <GLFW/glfw3.h>
+
+typedef struct
+{
+   GLfloat   m[4][4];
+} ESMatrix;
